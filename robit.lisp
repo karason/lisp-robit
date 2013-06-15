4e163de9dc4613a05a92521172338922d9b4a6e0
ryan karason <ryankarason@gmail.com>
2013-06-15 00:09:48 -0400

    added manual, todo, and bugs; integrated cl-irc with robit

--- robit.lisp
+++ robit.lisp
@@ -1,8 +1,44 @@
 ;robit.lisp
+
+(load "/home/karason/quicklisp/setup.lisp")
+
+(ql:quickload "cl-irc")
+
 (defpackage :robit
-    (:use :common-lisp))
+    (:use :common-lisp :irc)
+    (:export boot))
 
 (in-package :robit)
+
+(defparameter *nick* "rk[imposter]")
+
+(defparameter *server* "irc.freenode.net")
+
+(defparameter *channel* "#think")
+
+(defun boot ()
+    (defparameter *connection*
+        (connect
+            :nickname *nick*
+            :server *server*)) 
+    (join *connection* *channel*)
+    (add-hook *connection* 'irc::irc-privmsg-message 'ping-hook)
+    (ping-loop))
+
+(defun say (message)
+    (cond
+        ((eq message 'nil) 'nil)
+        (t (privmsg *connection* *channel* (string message)))))
+
+(defun ping-hook (ping)
+    (let
+        ((current-ping
+            (make-ping (get-universal-time) (source ping) (car (last (arguments ping))))))
+        (say (evaluate-ping current-ping))))
+
+ 
+(defun ping-loop ()
+    (read-message-loop *connection*))
 
 (defclass ping ()
     ((date :accessor ping-date
@@ -15,19 +51,6 @@
 (defun make-ping (date nick message)
     (make-instance 'ping :date date :nick nick :message message))
 
-(defparameter *current-ping* 
-    (make-ping (get-universal-time) 'ryankarason "hello world!"))
-
-(defun pretty-print (ping-object)
-    (princ (ping-date ping-object))
-    (princ " <")
-    (princ (ping-nick ping-object))
-    (princ "> ")
-    (princ (ping-message ping-object))
-    (princ #\newline))
-
-(pretty-print *current-ping*)
-
 (defun evaluate-ping (ping-object)
     (evaluate-message (ping-message ping-object)))
 
@@ -36,12 +59,7 @@
         ((search "(" message)
             (cond 
                 ((search ")" message :from-end t) 
-                    (eval (read-from-string 
-                        (subseq message (search "(" message) (+ 1 (search ")" message :from-end t))))))
+                    (write-to-string (ignore-errors (eval (read-from-string 
+                        (subseq message (search "(" message) (+ 1 (search ")" message :from-end t))))))))
                 (t nil)))
         (t nil)))
-
-(setf *current-ping* 
-    (make-ping (get-universal-time) 'ryankarason "hello robit, can you evaluate (+ 1 2 (- 3 (* 4 5))) for me?"))
-
-(princ (evaluate-ping *current-ping*))
